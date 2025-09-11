7f638d8841cd4d5791498533fccc2e56b057c07b
victorventuri <victorventuri95@gmail.com>
2025-09-11 15:04:30 -0500

    reorganization and more clear details on what is always available

---
+++ auto_feat/__init__.py
@@ -0,0 +1,35 @@
+"""
+Scripts to perform automatic featurization and model training
+"""
+from typing import Dict, List, Union
+
+
+class AutoFeaturizer():
+    """
+    Class that, given a user-specified target, some literature, and relevant data, employs LLMs to automatically
+    generates features to be used in downstream ML tasks.
+
+    Args:
+        papers: list of paths to where papers are stored (in raw text format)
+        data: path to where data is stored
+        target: user-specified target to be used by downstream ML models
+    """
+    def __init__(self,
+                 papers: List[str],
+                 data: Union[str],
+                 target: str) -> None:
+        pass
+
+    @property
+    def literature_review(self) -> str:
+        """
+        Returns a relevant summary of the paper(s) provided
+        """
+        pass
+
+    @property
+    def features_description(self) -> Dict:
+        """
+        Returns a dictionary containing physical descriptions of the features used and created thus far
+        """
+        pass
